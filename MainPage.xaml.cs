157309ce967f938aafeb4a8c0c831156ba089419
itodobien <daniel.w.garcia@outlook.com>
2025-05-27 13:40:01 -0700

    Minor Fixes
    Fixed a few typos
    Fixed and issue where not all text was visible in Knight's Tour rules was visible.
    Updated splash screen and AppIcon

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -28,6 +28,7 @@ public partial class MainPage : ContentPage
 
     private async void GoToKnightTour_OnClicked(object? sender, EventArgs e)
     {
+        
         try
         {
             await Shell.Current.GoToAsync("knighttour");
@@ -39,4 +40,20 @@ public partial class MainPage : ContentPage
     }
 
 
+    private void VADisclaimerButton_OnClicked(object? sender, EventArgs e)
+    {
+        DisclaimerPopup.Show();
+    }
+    
+    private async void ButtonBase_OnClicked(object? sender, EventArgs e)
+    {
+        try
+        {
+            await Launcher.OpenAsync(new Uri("https://www.va.gov"));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", "Could not open VA.gov website", "OK");
+        }
+    }
 }
